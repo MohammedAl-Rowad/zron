0bff13daa11bd7ce71ff2c19e2f7318b72e8ff8b
MohammedAl-rowad <rowadwo@gmail.com>
2021-11-20 14:10:11 +0200

    Add Tldraw

--- zron-client/src/App.js
+++ zron-client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Tldraw } from '@tldraw/tldraw'
+
+const App = () => {
+  const onChange = useCallback(
+    /**
+     * @param {import('@tldraw/tldraw').TldrawApp} app
+     */
+    (app, reason) => {
+      console.log(app.getAppState())
+    },
+    []
+  )
+
+  return <Tldraw onPersist={onChange} />
 }
 
-export default App;
+export default App
